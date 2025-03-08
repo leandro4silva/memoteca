c6340638d11d0632b58bd36aa28a9948ac48b530
Leandro Silva <leandro94.a.s@gmail.com>
2025-03-07 22:44:58 -0300

    feat: implement list thoughts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { FooterComponent } from './components/footer/footer.component';
 import {FormsModule} from "@angular/forms";
 import { ListThoughtsComponent } from './components/thoughts/list-thoughts/list-thoughts.component';
 import { CreateThoughtsComponent } from "./components/thoughts/create-thoughts/create-thoughts.component";
+import { ThoughtComponent } from './components/thoughts/thought/thought.component';
+import {NgOptimizedImage} from "@angular/common";
 
 @NgModule({
   declarations: [
@@ -16,12 +18,14 @@ import { CreateThoughtsComponent } from "./components/thoughts/create-thoughts/c
     FooterComponent,
     CreateThoughtsComponent,
     ListThoughtsComponent,
+    ThoughtComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        NgOptimizedImage
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/components/thoughts/list-thoughts/list-thoughts.component.ts
+++ src/app/components/thoughts/list-thoughts/list-thoughts.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListThoughtsComponent implements OnInit {
 
+  listThoughts = [
+    {
+      id: '1',
+      content: 'Teste',
+      author: 'Dev',
+      model: 'model1'
+    },
+    {
+      id: '2',
+      content: 'Teste 2',
+      author: 'Dev',
+      model: 'model2'
+    },
+    {
+      id: '3',
+      content: 'Teste 3',
+      author: 'Dev',
+      model: 'model3'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
