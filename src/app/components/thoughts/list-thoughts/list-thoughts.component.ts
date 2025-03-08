78648ee1874b22630ad373be9657e0a39fec4b34
Leandro Silva <leandro94.a.s@gmail.com>
2025-03-07 23:58:07 -0300

    feat: resizing thought cards

--- src/app/components/thoughts/list-thoughts/list-thoughts.component.ts
+++ src/app/components/thoughts/list-thoughts/list-thoughts.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Thought{
+  id: string
+  content: string
+  author: string
+  model: string
+}
+
 @Component({
   selector: 'app-list-thoughts',
   templateUrl: './list-thoughts.component.html',
@@ -7,10 +14,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListThoughtsComponent implements OnInit {
 
-  listThoughts = [
+  listThoughts : Array<Thought> = [
     {
       id: '1',
-      content: 'Teste',
+      content: '@lorem ipsum @lorem ipsum@lorem ipsum @lorem ipsum @lorem ipsum  @lorem ipsum @lorem ipsum @lorem ipsum @lorem ipsum @lorem ipsum ' +
+        '@lorem ipsum @lorem ipsum@lorem ipsum @lorem ipsum @lorem ipsum  @lorem ipsum @lorem ipsum @lorem ipsum @lorem ipsum @lorem ipsum ' +
+      '@lorem ipsum @lorem ipsum@lorem ipsum @lorem ipsum @lorem ipsum  @lorem ipsum @lorem ipsum @lorem ipsum @lorem ipsum @lorem ipsum ',
       author: 'Dev',
       model: 'model1'
     },
@@ -25,11 +34,44 @@ export class ListThoughtsComponent implements OnInit {
       content: 'Teste 3',
       author: 'Dev',
       model: 'model3'
-    }
+    },
+    {
+      id: '2',
+      content: 'Teste 2',
+      author: 'Dev',
+      model: 'model2'
+    },
+    {
+      id: '3',
+      content: 'Teste 3',
+      author: 'Dev',
+      model: 'model3'
+    },
+    {
+      id: '3',
+      content: 'Teste 3',
+      author: 'Dev',
+      model: 'model3'
+    },
+    {
+      id: '1',
+      content: '@lorem ipsum @lorem ipsum@lorem ipsum @lorem ipsum @lorem ipsum  @lorem ipsum @lorem ipsum @lorem ipsum @lorem ipsum @lorem ipsum ' +
+        '@lorem ipsum @lorem ipsum@lorem ipsum @lorem ipsum @lorem ipsum  @lorem ipsum @lorem ipsum @lorem ipsum @lorem ipsum @lorem ipsum ' +
+        '@lorem ipsum @lorem ipsum@lorem ipsum @lorem ipsum @lorem ipsum  @lorem ipsum @lorem ipsum @lorem ipsum @lorem ipsum @lorem ipsum ',
+      author: 'Dev',
+      model: 'model1'
+    },
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  widthThought(thought: Thought): string{
+    if(thought.content.length >= 256){
+      return 'thought-g'
+    }
+    return 'thought-p'
   }
 }
